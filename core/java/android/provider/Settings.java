9ba2c174f3c1a957e2e102e3aadafb557f10e90b
Lucchetto <lucchetto.tie@live.com>
2019-09-20 12:16:18 +0200

    [1/2] base: Double tap to sleep on lockscreen
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4859,6 +4859,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
+                "double_tap_sleep_lockscreen";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -4988,6 +4995,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
             PRIVATE_SETTINGS.add(FP_UNLOCK_KEYSTORE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
         }
 
         /**
