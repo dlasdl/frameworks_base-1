e29994d53a87180f1831db8f599336a6dde43692
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-15 14:56:06 +0200

    base: Allow toggling QS media player [1/2]
    And make it play nicely with our current features
    Change-Id: I8735c3b2c23b58cb420ad20d74fe91ed31a31f0e
    
    Change-Id: Id80823c7ccab12f8cf3bb77a20ebcd2e9769b741
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4836,6 +4836,12 @@ public final class Settings {
         public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
 
         /**
+        * Whether to show media player in QS
+        * @hide
+        */
+        public static final String QS_MEDIA_PLAYER = "qs_media_player";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
