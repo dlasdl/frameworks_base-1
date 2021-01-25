ab38ced1b9bf657a898617b93aaca8a8efa3c542
idoybh2 <idoybh2@gmail.com>
2021-01-25 17:04:06 +0000

    base: Allow toggling QS media player [1/2]
    Change-Id: Id80823c7ccab12f8cf3bb77a20ebcd2e9769b741
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5065,6 +5065,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether to show media player in QS
+         * @hide
+         */
+        public static final String QS_MEDIA_PLAYER = "qs_media_player";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
