11107c5c82eb02245543c87cc8a9f6cd490295a1
ugly-kid-af <dwarmachine24@gmail.com>
2021-02-13 12:04:39 +0530

    Revert "base: Smart Charging (1/4)"
    This reverts commit 060f8f1546ff10bf96a84087ba783d122ff6d542.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4822,16 +4822,6 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
-         * @hide
-         */
-        public static final String SMART_CHARGING = "smart_charging";
-
-        /**
-         * @hide
-         */
-        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
