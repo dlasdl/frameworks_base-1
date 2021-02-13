8792ddaec573a985df9aa8c3b98da209cedaa8b4
ugly-kid-af <dwarmachine24@gmail.com>
2021-02-13 12:03:03 +0530

    Revert "SmartCharging: add reset battery stats option [1/2]"
    This reverts commit d346d99093c2f6211d016c681ce0c6711b546372.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4829,11 +4829,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
-
-        /**
-         * @hide
-         */
         public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
 
         /**
