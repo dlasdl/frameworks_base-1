BlissRoms/platform_frameworks_base

SystemUI: Sfuny: Fix NPE on setDarkAmount.
Change-Id: I40a94a893f89a2af4787e0e08795b524a4d6c0aa

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -70,6 +70,11 @@ public class SfunyClockController implements ClockPlugin {
     private TextClock mMinuteClock;
 
     /**
+     * Controller for transition into dark state.
+     */
+    private CrossFadeDarkController mDarkController;
+
+    /**
      * Create a DefaultClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -168,7 +173,9 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setDarkAmount(float darkAmount) {
-        mBigClockView.setDarkAmount(darkAmount);
+        if (mDarkController != null) {
+            mBigClockView.setDarkAmount(darkAmount);
+        }
     }
 
     @Override

dlasdl/frameworks_base-1

Signed-off-by: smokey18 <kmboplays@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -73,6 +73,11 @@ public class SfunyClockController implements ClockPlugin {
     private TextClock mMinuteClock;
 
     /**
+     * Controller for transition into dark state.
+     */
+    private CrossFadeDarkController mDarkController;
+
+    /**
      * Create a DefaultClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -171,6 +176,9 @@ public class SfunyClockController implements ClockPlugin {
     @Override
     public void setDarkAmount(float darkAmount) {
         mView.setDarkAmount(darkAmount);
+        if (mDarkController != null) {
+            mView.setDarkAmount(darkAmount);
+        }
     }
 
     @Override